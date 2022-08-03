cdacf661b055bf7f1f6772817fab3511b4f6d88e
Cory Pollard <astandard87@gmail.com>
2022-08-03 13:28:05 -0600

    Added project files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import NasaPhoto from './components/NasaPhoto';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Home />} path="/" exact />
+        <Route element={<NasaPhoto />} path="/nasaphoto" />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/NasaPhoto.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+
+const API_KEY = process.env.REACT_APP_NASA_KEY;
+
+const NasaPhoto = () => {
+  const [photoData, setPhotoData] = useState(null);
+
+  useEffect(() => {
+    const fetchPhoto = async () => {
+      try {
+        const res = await fetch(
+          `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
+        );
+
+        if (!res.ok) throw new Error('A problem has occured!');
+
+        const data = await res.json();
+        console.log(data);
+        setPhotoData(data);
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+    fetchPhoto();
+  }, []);
+  if (!photoData) return <div />;
+
+  return (
+    <>
+      {photoData.media_type === 'image' ? (
+        <img src={photoData.url} alt={photoData.title} />
+      ) : (
+        <iframe
+          title="space-video"
+          src={photoData.url}
+          frameBorder="0"
+          gesture="media"
+          allow="encrypted-media"
+          allowFullScreen
+          className="photo"
+        />
+      )}
+      <div>
+        <h1>{photoData.title}</h1>
+        <p>{photoData.date}</p>
+        <p>{photoData.explanation}</p>
+      </div>
+    </>
+  );
+};
+
+export default NasaPhoto;
